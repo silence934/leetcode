a81179fb1aa9586563ad6acf76ad8da5aded70a0
silence <fucong@beisen.com>
2020-10-18 19:52:24 +0800

    搜狗笔试

--- src/main/java/xyz/nyist/leetcode/to600/Solution416.java
+++ src/main/java/xyz/nyist/leetcode/to600/Solution416.java
@@ -36,21 +36,15 @@ public class Solution416 {
             return false;
         }
         sum /= 2;
-        boolean[][] dp = new boolean[nums.length + 1][sum + 1];
-        for (int i = 0; i < nums.length + 1; i++) {
-            dp[i][0] = true;
-        }
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+
         for (int i = 1; i < nums.length + 1; i++) {
             for (int j = 1; j < sum + 1; j++) {
-                if (nums[i - 1] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                }
-                else {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                }
+                dp[j] = dp[j] || dp[j - nums[i - 1]];
             }
         }
-        return dp[nums.length][sum];
+        return dp[sum];
     }
 
 }
---
+++ src/main/java/xyz/nyist/writerTest/xiaomi/Test2.java
@@ -0,0 +1,52 @@
+package xyz.nyist.writerTest.xiaomi;
+
+import xyz.nyist.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Author : fucong
+ * @Date: 2020-09-27 09:47
+ * @Description :
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Test2 test2 = new Test2();
+        System.out.println(test2.sumNumbers(null));
+    }
+
+    public int sumNumbers(TreeNode root) {
+        List<String> list = test(root);
+        int res = 0;
+        for (String s : list) {
+            res += Integer.parseInt(s);
+        }
+        return res;
+    }
+
+    public List<String> test(TreeNode root) {
+        if (root == null) {
+            return Collections.singletonList("0");
+        }
+        List<String> res = new ArrayList<>();
+        if (root.left != null) {
+            List<String> l = test(root.left);
+            for (String s : l) {
+                res.add("" + root.val + s);
+            }
+        }
+        if (root.right != null) {
+            List<String> r = test(root.right);
+            for (String s : r) {
+                res.add("" + root.val + s);
+            }
+        }
+
+        if (root.left == null && root.right == null) {
+            res.add("" + root.val);
+        }
+        return res;
+    }
+}
