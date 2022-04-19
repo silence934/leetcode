4bbb917f94c954e9c7bab7abb3f40cc274e91d06
fucong <fucong@tsingj.com>
2022-04-19 18:13:32 +0800

    前缀树

--- src/main/java/xyz/nyist/leetcode/begin200/Solution208.java
+++ src/main/java/xyz/nyist/leetcode/begin200/Solution208.java
@@ -7,20 +7,55 @@ package xyz.nyist.leetcode.begin200;
  */
 public class Solution208 {
 
-    public Solution208() {
+    private Solution208[] children;
 
+    private boolean isEnd;
+
+    public Solution208() {
+        children = new Solution208[26];
+        isEnd = false;
     }
 
     public void insert(String word) {
+        insert(word, 0);
+    }
 
+    private void insert(String word, int start) {
+        if (word.length() == start) {
+            isEnd = true;
+            return;
+        }
+        char c = word.charAt(start);
+        Solution208 node;
+        if ((node = children[c - 'a']) == null) {
+            node = new Solution208();
+            children[c - 'a'] = node;
+        }
+        node.insert(word, start + 1);
     }
 
     public boolean search(String word) {
-        return false;
+        return search(word, 0);
+    }
+
+    private boolean search(String prefix, int index) {
+        if (prefix.length() == index) {
+            return isEnd;
+        }
+        Solution208 node = children[prefix.charAt(index) - 'a'];
+        return node != null && node.search(prefix, index + 1);
     }
 
     public boolean startsWith(String prefix) {
-        return false;
+        return startsWith(prefix, 0);
+    }
+
+    private boolean startsWith(String prefix, int index) {
+        if (prefix.length() == index) {
+            return true;
+        }
+        Solution208 node = children[prefix.charAt(index) - 'a'];
+        return node != null && node.startsWith(prefix, index + 1);
     }
 
 }
---
+++ src/main/java/xyz/nyist/leetcode/begin200/Solution211.java
@@ -0,0 +1,72 @@
+package xyz.nyist.leetcode.begin200;
+
+/**
+ * @author: fucong
+ * @Date: 2022/4/18 17:53
+ * @Description:
+ */
+public class Solution211 {
+
+    private Solution211[] children;
+
+    private boolean isEnd;
+
+    public Solution211() {
+        isEnd = false;
+        children = new Solution211[26];
+    }
+
+    public void addWord(String word) {
+        addWord(word, 0);
+    }
+
+    private void addWord(String word, int index) {
+        if (index == word.length()) {
+            isEnd = true;
+            return;
+        }
+        char c = word.charAt(index);
+        if (c == '.') {
+            for (int i = 0; i < 26; i++) {
+                Solution211 node;
+                if ((node = children[i]) == null) {
+                    node = new Solution211();
+                    children[i] = node;
+                }
+                node.addWord(word, index + 1);
+            }
+
+        } else {
+            Solution211 node;
+            if ((node = children[c - 'a']) == null) {
+                node = new Solution211();
+                children[c - 'a'] = node;
+            }
+            node.addWord(word, index + 1);
+        }
+    }
+
+
+    public boolean search(String word) {
+        return search(word, 0);
+    }
+
+    private boolean search(String prefix, int index) {
+        if (prefix.length() == index) {
+            return isEnd;
+        }
+        char c = prefix.charAt(index);
+        if (c == '.') {
+            for (Solution211 child : children) {
+                if (child != null && child.search(prefix, index + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            Solution211 node = children[c - 'a'];
+            return node != null && node.search(prefix, index + 1);
+        }
+    }
+
+}
