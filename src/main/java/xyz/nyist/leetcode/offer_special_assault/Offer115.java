1436097e960b10498e79d3d9455bba7205651c9a
fucong <fucong@tsingj.com>
2022-09-14 15:11:20 +0800

    file

---
+++ src/main/java/xyz/nyist/leetcode/offer_special_assault/Offer115.java
@@ -0,0 +1,9 @@
+package xyz.nyist.leetcode.offer_special_assault;
+
+/**
+ * @author: silence
+ * @Date: 2022/9/14 15:08
+ * @Description:
+ */
+public class Offer115 {
+}
---
+++ src/main/java/xyz/nyist/leetcode/offer_special_assault/Offer21.java
@@ -0,0 +1,22 @@
+package xyz.nyist.leetcode.offer_special_assault;
+
+import xyz.nyist.utils.ListNode;
+
+import java.io.IOException;
+
+/**
+ * @author: silence
+ * @Date: 2022/9/14 15:06
+ * @Description:
+ */
+public class Offer21 {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+
+}
