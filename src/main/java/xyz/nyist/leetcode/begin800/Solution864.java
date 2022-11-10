46b60a9fcc0ffdc11af59ae822276b5797821ce0
silence934 <49453669+silence934@users.noreply.github.com>
2022-11-10 15:59:10 +0800

    temp

--- src/main/java/xyz/nyist/leetcode/begin800/Solution864.java
+++ src/main/java/xyz/nyist/leetcode/begin800/Solution864.java
@@ -1,5 +1,8 @@
 package xyz.nyist.leetcode.begin800;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author: silence
  * @Date: 2022/11/10 15:30
@@ -8,9 +11,64 @@ package xyz.nyist.leetcode.begin800;
 public class Solution864 {
 
     public int shortestPathAllKeys(String[] grid) {
-        
 
-        return 1;
+        int keyNumber = 0;
+
+        int x = -1;
+        int y = -1;
+
+        char[][] map = new char[grid.length][grid[0].length()];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length(); j++) {
+                char c = grid[i].charAt(j);
+                if (Character.isLowerCase(c)) {
+                    keyNumber = Math.max(keyNumber, c - 'a');
+                }
+                if (c == '@') {
+                    x = i;
+                    y = j;
+                }
+            }
+            map[i] = grid[i].toCharArray();
+        }
+
+
+        keyNumber++;
+
+
+        Set<Character> keys = new HashSet<>();
+
+        return dfs(keys, map, x, y, keyNumber, 0);
     }
+
+
+    private int dfs(Set<Character> keys, char[][] map, int x, int y, int keyNumber, int step) {
+        if (x < 0 || x >= map.length || y < 0 || y >= map[x].length) {
+            return Integer.MAX_VALUE;
+        }
+        if (keys.size() == keyNumber) {
+            return step;
+        }
+        char c = map[x][y];
+
+        if (c == '#' || c == '0') {
+            return Integer.MAX_VALUE;
+        }
+
+        if (Character.isUpperCase(c)) {
+            if (keys.contains(c)) {
+
+            } else {
+                return Integer.MAX_VALUE;
+            }
+        }
+
+        if (Character.isLowerCase(c)) {
+            keys.add(Character.toUpperCase(c));
+        }
+
+    }
+
 
 }
